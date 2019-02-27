a2d9c7ff0d8a94e9059d30e0046d988d5a8c3479
Paul PRIKAZSKY <pprikazs@student.42.fr>
2019-02-27 13:30:31 +0100

    Change the default path

--- lib_srcs.sh
+++ lib_srcs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-export DYLD_INSERT_LIBRARIES="/Users/pprikazs/Documents/Project/libft_malloc/libft_malloc.so"
+export DYLD_INSERT_LIBRARIES="/Absolut/path/to/.so"
 export DYLD_FORCE_FLAT_NAMESPACE=1
 $@
